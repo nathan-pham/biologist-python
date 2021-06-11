30838225de068b9916d3f1097f709b717dcad08d
Nathan Pham <nathanpham.me@gmail.com>
2021-06-11 10:26:29 -0700

    comments

--- chapter-2/generate_kmers.py
+++ chapter-2/generate_kmers.py
@@ -5,13 +5,16 @@ def generate_kmers(length):
     bases = ['A', 'T', 'G', 'C']
     result = ['']
 
+    # repeat for the length of kmers
     for i in range(length):
         new_result = []
 
+        # loop through existing results & add a base to it
         for kmer in result:
             for base in bases:
                 new_result.append(kmer + base)
 
+        # set result to new result
         result = new_result
 
     return result
@@ -19,11 +22,13 @@ def generate_kmers(length):
 def generate_kmers_recursively(length):
     bases = ['A', 'T', 'G', 'C']
 
+    # if length is 1 then return default bases
     if length == 1:
         return bases
     else:
         result = []
 
+        # create a sequence one less than the length & add all of the bases to it
         for sequence in generate_kmers_recursively(length - 1):
             for base in bases:
                 result.append(sequence + base)
