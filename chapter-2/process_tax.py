f8b1fcccb7f4fa3e0edc0a564c0486d3bd04cc2b
Nathan Pham <nathanpham.me@gmail.com>
2021-06-11 10:49:44 -0700

    log process

--- chapter-2/process_tax.py
+++ chapter-2/process_tax.py
@@ -41,14 +41,29 @@ def get_ancestors(taxon):
 
     return result
 
-def get_ancestors_recursively(taxon):
+def get_ancestors_recursively(taxon, depth):
+    spacer = " " * depth * 4
+
+    def s_print(string):
+        print(f"{spacer} {string}")
+    
+    s_print(f"calculating ancestors for taxon {taxon}")
+
     if taxon == "Primates":
+        s_print("taxon == Primates, returning empty list")
         return []
     else:
+        s_print("taxon != Primates, calculating parent")
         parent = tax_dict.get(taxon)
-        parent_ancestors = get_ancestors_recursively(parent)
-        return [parent] + parent_ancestors
+        s_print(f"calculating ancestors for parent {parent}")
+
+        parent_ancestors = get_ancestors_recursively(parent, depth + 1)
+        s_print(f"parent ancestors are {str(parent_ancestors)}")
+
+        result = [parent] + parent_ancestors
+        s_print(f"returning result {str(result)}")
+        return result
 
 print(get_ancestors("Galago alleni"))
-print(get_ancestors_recursively("Galago alleni"))
+print(get_ancestors_recursively("Galago alleni", 0))
 
