eb3349d83a21a316dabad22eec43c95326bbc235
Nathan Pham <nathanpham.me@gmail.com>
2021-06-11 10:21:02 -0700

    recursive method

--- chapter-2/generate_kmers.py
+++ chapter-2/generate_kmers.py
@@ -6,10 +6,32 @@ def generate_kmers(length):
     result = ['']
 
     for i in range(length):
-        for j in range(len(result)):
+        new_result = []
+
+        for kmer in result:
             for base in bases:
-                result[j] += base
+                new_result.append(kmer + base)
+
+        result = new_result
 
     return result
 
+def generate_kmers_recursively(length):
+    bases = ['A', 'T', 'G', 'C']
+
+    if length == 1:
+        return bases
+    else:
+        result = []
+
+        for sequence in generate_kmers_recursively(length - 1):
+            for base in bases:
+                result.append(sequence + base)
+
+        return result
+
+# imperative method
 print(generate_kmers(3))
+
+# recursive method
+print(generate_kmers_recursively(3))
