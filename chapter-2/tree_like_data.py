d359177f8f3c328eccd1287d6598fc377e469fbe
Nathan Pham <nathanpham.me@gmail.com>
2021-06-11 10:37:31 -0700

    imperative ancestor

--- chapter-2/tree_like_data.py
+++ chapter-2/tree_like_data.py
@@ -1,5 +1,6 @@
 """
 Primate Taxa
+
 Primates
     Haplorrhini
         Simiiformes
@@ -17,3 +18,26 @@ Primates
             Galago alleni
             Galago moholi
 """
+
+taxonomy = { 
+    "Pan troglodytes": "Hominoidea", "Pongo abelii": "Hominoidea", 
+    "Hominoidea": "Simiiformes", "Simiiformes": "Haplorrhini", 
+    "Tarsius tarsier": "Tarsiiformes", "Haplorrhini": "Primates",
+    "Tarsiiformes": "Haplorrhini", "Loris tardigradus": "Lorisidae",
+    "Lorisidae": "Strepsirrhini", "Strepsirrhini": "Primates",
+    "Allocebus trichotis": "Lemuriformes", "Lemuriformes": "Strepsirrhini",
+    "Galago alleni": "Lorisiformes", "Lorisiformes": "Strepsirrhini",
+    "Galago moholi": " Lorisiformes"
+} 
+
+def get_ancestors(taxon):
+    result = []
+
+    while taxon != "Primates":
+        ancestor = taxonomy.get(taxon)
+        result.append(ancestor)
+        taxon = ancestor
+
+    return result
+
+print(get_ancestors("Galago alleni"))
