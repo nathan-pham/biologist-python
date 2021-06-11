2a2da8e572e64bbfd0bbcb8c3527bfd6003a35b4
Nathan Pham <nathanpham.me@gmail.com>
2021-06-11 10:30:53 -0700

    primate taxa

---
+++ chapter-2/tree_like_data.py
@@ -0,0 +1,19 @@
+"""
+Primate Taxa
+Primates
+    Haplorrhini
+        Simiiformes
+            Hominoidea
+                Pan troglodytes
+                Pongo abelii
+        Tarsiiformes
+            Tarsius tarsier
+    Strepsirrhini
+        Lorisidae
+            Loris tardigradus
+        Lemuriformes
+            Allocebus trichotis
+        Lorisiformes
+            Galago alleni
+            Galago moholi
+"""
