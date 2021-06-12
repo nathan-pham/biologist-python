a05203e23e8b23dfc6bf5f4cbe8ed84566fa9539
Nathan Pham <nathanpham.me@gmail.com>
2021-06-12 06:14:59 -0700

    new tax dict

---
+++ chapter-2/tree-like-data/parent_child_tree.py
@@ -0,0 +1,11 @@
+tax_dict = {
+    "Primates": ["Haplorrhini", "Strepsirrhini"], 
+    "Tarsiiformes": ["Tarsius tarsier"], 
+    "Haplorrhini": ["Tarsiiformes", "Simiiformes"], 
+    "Simiiformes": ["Hominoidea"], 
+    "Lorisidae": ["Loris tardigradus"], 
+    "Lemuriformes": ["Allocebus trichotis"], 
+    "Lorisiformes": ["Galago alleni","Galago moholi"], 
+    "Hominoidea": ["Pongo abelii", "Pan troglodytes"], 
+    "Strepsirrhini": ["Lorisidae", "Lemuriformes", "Lorisiformes"] 
+}
