44a823b24f181158a012924400098a7ae95979d7
Nathan Pham <nathanpham.me@gmail.com>
2021-06-12 06:23:23 -0700

    recursive child/parent

--- chapter-2/tree-like-data/parent_child_tree.py
+++ chapter-2/tree-like-data/parent_child_tree.py
@@ -9,3 +9,27 @@ tax_dict = {
     "Hominoidea": ["Pongo abelii", "Pan troglodytes"], 
     "Strepsirrhini": ["Lorisidae", "Lemuriformes", "Lorisiformes"] 
 }
+
+def get_children(taxon):
+    stack = [taxon]
+    result = []
+
+    while len(stack) != 0:
+        current = stack.pop()
+        current_children = tax_dict.get(current, [])
+        stack += current_children
+        result.append(current)
+
+    return result
+
+def get_children_recursively(taxon):
+    result = [taxon]
+    children = tax_dict.get(taxon, [])
+
+    for child in children:
+        result += get_children_recursively(child)
+
+    return result
+
+print(get_children("Strepsirrhini"))
+print(get_children_recursively("Strepsirrhini"))
