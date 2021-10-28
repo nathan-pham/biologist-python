fa03ce49fa0b06badd000f66acf9973d0a702ec2
Nathan Pham <nathanpham.me@gmail.com>
2021-10-28 13:01:01 -0700

    includes with sublists

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Python and biologist applications
     * Processing Tree-like Data
         * [Child-to-parent Trees](/chapter-2/child_parent.py)
         * [Parent-to-child Trees](/chapter-2/parent_child.py)
+
+* Chapter 3: Complex Data Structures
+    * [Complex Structures](/chapter-3/data_structures.py)
+    
--- chapter-3/tree_lists.py
+++ chapter-3/tree_lists.py
@@ -7,6 +7,7 @@ tree = ['dog',
     ]
 ]
 
+# check if element is present in a tree-like list
 def includes(tree, element):
     for leaf in tree:
         if leaf == element or (includes(leaf, element) if isinstance(leaf, list) else False):
@@ -14,4 +15,19 @@ def includes(tree, element):
 
     return False
 
-print(includes(tree, "seals"))
+print(includes(tree, "seal"))
+
+# retrieve sublists that contain a taxon
+def sublist(tree, taxon):
+    results = []
+
+    if includes(tree, taxon):
+        results.append(tree)
+
+    for leaf in tree:
+        if isinstance(leaf, list):
+            results.extend(sublist(leaf, taxon))
+
+    return results
+
+print("\n".join([f"{ leaf } includes monkey" for leaf in sublist(tree, "monkey")]))
